1bcb2814ec8b11649340d7f7f82f5fc58cfbcb1d
sgPepper <quentin.bouvet@hotmail.fr>
2016-07-19 16:56:57 +0200

    fixed bug preventing execution

--- 1.testbenchAndDoScripts/elasticbuffer.tb.vhd
+++ 1.testbenchAndDoScripts/elasticbuffer.tb.vhd
@@ -10,23 +10,12 @@ end TB_EB;
 architecture testbench of TB_EB is
 
 	signal finished    : boolean := false;
-    signal currenttime : time    := 0 ns;
-	
-	constant CLK_PERIOD : time := 10 ns;
-
-	component elasticBuffer is
-	port(
-		clk, reset : in std_logic;
-		
-		d_in : in std_logic_vector(31 downto 0);
-		d_out : out std_logic_vector(31 downto 0);
-		
-		p_valid, n_ready : in std_logic;
-		ready, valid : out std_logic);
-	end component;
-	
+    signal currenttime : time    := 0 ns;	
     signal reset : std_logic := '1';
 	signal clk : std_logic := '0';
+	
+	constant CLK_PERIOD : time := 10 ns;
+	
 	signal p_valid, n_ready : std_logic := '0';
 	signal d_in : std_logic_vector(31 downto 0) := (others => '0');
 	
@@ -36,6 +25,7 @@ architecture testbench of TB_EB is
 begin
 
 	simulation : process	
+	
 		procedure reset_sim is 
 		begin
 			wait until rising_edge(clk);
@@ -58,6 +48,7 @@ begin
 			console_out := new string'(msg);
 			writeline(output, console_out);
 		end procedure print;
+		
 	begin if(not finished) then
 		reset_sim;
 		
@@ -94,9 +85,9 @@ begin
 		n_ready <= '1';
 		d_in <= X"00000007";
 		wait for CLK_PERIOD;
-		p_valid <= '0';
 		assert d_out = X"00000006" report "incorrect output (2)" severity error;
-		wait for CLK_PERIOD;
+		wait for CLK_PERIOD;		
+		p_valid <= '0';
 		assert d_out = X"00000007" report "incorrect output (3)" severity error;
 		reset_sim;
 		
@@ -117,7 +108,7 @@ begin
 	end process simulation;
 
 	--instanciating design under test
-	DUT : elasticBuffer port map(
+	DUT : entity work.ElasticBuffer_reg port map(
 		clk, reset, d_in, d_out, 
 		p_valid, n_ready, ready, valid
 		);
