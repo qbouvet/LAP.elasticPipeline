39d823b8bfbbf55788160780dc8a6a0f8ae66bdd
sgPepper <quentin.bouvet@hotmail.fr>
2016-07-20 16:46:41 +0200

    finished version, tests seem quite complete, antitokenChannel component passes the tests

--- 1.testbenchAndDoScripts/antitokenChannel.tb.vhd
+++ 1.testbenchAndDoScripts/antitokenChannel.tb.vhd
@@ -65,8 +65,7 @@ begin
 		if(finished=false)then
 			
 			--simple token with constant enable
-			newline;print("simple 1-antitoken test with constant enable")
-			wait for CLK_PERIOD;
+			newline;print("simple 1-antitoken test with constant p_valid/n_ready - latency = 3 cycles");
 			antitoken <= '1';
 			p_valid <= '1';
 			n_ready <= '1';
@@ -78,11 +77,77 @@ begin
 			wait for CLK_PERIOD;
 			assert valid = '1' report "'valid' signal should be back up" severity error;
 			
+			--several tokens with interrupted p_valid/n_read signals
+			newline;print("several tokens with interrupted p_valid/n_read signals - latency = 5 cycles");
+			resetSim;
+			latency <= "101";
+			antitoken <= '1';
+			p_valid <= '1';
+			n_ready <= '1'; 
+			waitPeriod(3); --let's load 3 consecutive tokens, then test if the cycle count stops correctly accroding to p_valid and n_ready
+			antitoken <= '0';
+			n_ready <= '0';
+			waitPeriod(5); -- should be stopped
+			assert ready = '0' report "the channel should wait on the next buffer's ready signal" severity error;
+			assert valid = '1' report "p_valid is still high, so valid must be too" severity error;
+			p_valid <= '0';
+			n_ready <= '1';
+			waitPeriod(5); -- should still be stopped
+			assert ready = '1' report "the buffer following the channel is ready, channel should report ready" severity error;
+			assert valid = '0' report "no valid data incomming, shoud be not ready" severity error;
+			p_valid <= '1';
+			waitPeriod(2); -- now should have completed shifting, the first antitoken should have reached the last buffer
+			assert ready='1' report "timeout reached, channel should accept any data to discard it asap" severity error;
+			assert valid='0' report "timeout reached, until enough data discarded, channel should not send valid data" severity error;
+			waitPeriod(2); -- discard data 2 times and let 3 out of 3 antitokens out
+			p_valid <= '0';
+			waitPeriod(2); -- stopping incomming valid data should stop the shifting
+			assert ready='1' report "still in timeout, channel should accept any data to discard it asap" severity error;
+			assert valid='0' report "still in timeout, until enough data discarded, channel should not send valid data" severity error;
+			p_valid <= '1';
+			waitPeriod(1); --let the last antiToken out
+			assert ready=n_ready report "should be back to normal" severity error;
+			assert valid=p_valid report "should be back to normal" severity error;
+			n_ready <='0';
+			p_valid <= '0';
+			waitPeriod(1); --let's quickly test ready/valid signals for correct behaviour without antitokens
+			assert ready=n_ready report "should be back to normal (2)" severity error;
+			assert valid=p_valid report "should be back to normal (2)" severity error;
+			waitPeriod(3);
 			
+			--continuous flow of antitokens block the channel
+			newline;print("continuous flow of antitokens blocks the channel's valid='0' and ready='1' - latency 3");
+			resetSim;
+			latency <= "011";
+			antitoken <= '1'; 
+			p_valid <= '1';
+			n_ready <= '1';
+			-- should wait before asserting signals assigned just before
+			--assert ready = '1' report "not blocked yet, n_ready should pass through" severity error;
+			--assert valid = '1' report "not blocked yet, n_ready should pass through" severity error;
+			waitPeriod(1);
+			assert ready = '1' report "not blocked yet, n_ready should pass through" severity error;
+			assert valid = '1' report "not blocked yet, n_ready should pass through" severity error;
+			waitPeriod(1);
+			assert ready = '1' report "not blocked yet, n_ready should pass through" severity error;
+			assert valid = '1' report "not blocked yet, n_ready should pass through" severity error;
+			waitPeriod(1); --AT shifted to last register
+			assert ready ='1' report "should block" severity error;
+			assert valid='0' report "should block" severity error;
+			n_ready<='0';
+			p_valid<= '1';
+			assert ready ='1' report "should block" severity error;
+			assert valid='0' report "should block" severity error;
+			waitPeriod(1); --AT shifted to last register
+			assert ready ='1' report "should block" severity error;
+			assert valid='0' report "should block" severity error;
+			
+			
+			waitPeriod(3);		
+			newline; newline;
 			assert false report "finished simulation" severity note;
 			
 		end if;
-		waitPeriod(10);
 		finished <= true;
 	end process;
 	
