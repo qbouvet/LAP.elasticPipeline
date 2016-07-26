f2f2b503e44c93b91db11ea12e7f825111f9b5bd
sgPepper <quentin.bouvet@hotmail.fr>
2016-07-26 15:49:22 +0200

    quickly tested delay channel

--- 0.components/delayChannel.vhd
+++ 0.components/delayChannel.vhd
@@ -5,7 +5,11 @@
 -- also outputs each piece of data in the buffers in the 
 -- data_out array of std_logic_vectors to be able to bypass 
 -- unnecessary buffers.
+-- on the output signal 'signal', signal(i) is the signal of delay i
+-- NB i=0 -> stright input signal
 ---------------------------------------------------------------------
+
+
 library ieee;
 use ieee.std_logic_1164.all;
 use work.customTypes.all;
@@ -16,40 +20,44 @@ generic( 	DATA_SIZE : integer;
 port(	clk, reset : in std_logic;
 		data_in : in std_logic_vector(DATA_SIZE-1 downto 0);
 		-- all the delayed outputs
-		data_out : out vectorArray_t(0 to DELAY)(DATA_SIZE-1 DOWNTO 0);
+		data_out : out vectorArray_t(0 to DELAY)(DATA_SIZE-1 DOWNTO 0); -- we want data_out(0) to select the signal with 0 delay
 		--elastic control signals
 		p_valid, n_ready : in std_logic;
-		ready, valid : out std_logic
+		ready : out std_logic;
+		valid : out bitArray_t(0 to DELAY) -- same as data_out
 ); 
 end delayChannel;
+
 
 architecture generique of delayChannel is
 	type bitArray is array(integer range <>) of std_logic;
 	--signals used to link the buffers together	
 	signal data : vectorArray_t (DELAY-1 downto 0)(DATA_SIZE-1 downto 0);
-	signal buffer_ready : bitArray(DELAY-1 downto 1);
-	signal buffer_valid : bitArray(DELAY-1 downto 1);
+	signal buffer_ready : bitArray_t(DELAY-1 downto 1); -- only between two buffers from the chain
+	signal buffer_valid : bitArray_t(DELAY-1 downto 0); -- we need one more to map the valid signal of the last buffer
 begin
 
 	-- instantiate the chain of buffers
 	inputBuffer : entity work.elasticBuffer_reg generic map(DATA_SIZE)
 			port map(clk, reset, data_in, data(DELAY-1), p_valid, buffer_ready(DELAY-1), ready, buffer_valid(DELAY-1));
 	
-	genBuffers : for n in DELAY-1 downto 1 generate
+	genBuffers : for n in DELAY-1 downto 2 generate
 		intermediateBuffer : entity work.elasticBuffer_reg generic map(DATA_SIZE)
 				port map (clk, reset, data(n), data(n-1), buffer_valid(n), buffer_ready(n-1), buffer_ready(n), buffer_valid(n-1));
 	end generate;
 	
 	outputBuffer : entity work.elasticBuffer_reg generic map(DATA_SIZE)
-			port map(clk, reset, data(1), data(0), buffer_valid(1), n_ready, buffer_ready(1), valid);
+			port map(clk, reset, data(1), data(0), buffer_valid(1), n_ready, buffer_ready(1), buffer_valid(0));
 	
 	
-	-- map the delayed output signals to the intermediate signals linking the buffers	
-	process(data)
+	-- map the delayed data and control output signals to the intermediate signals linking the buffers	
+	process(data, data_in, clk, reset, p_valid)
 	begin
 		data_out(0) <= data_in;
+		valid(0) <= p_valid;
 		for n in 1 to DELAY loop
 			data_out(n) <= data(DELAY-n);
+			valid(n) <= buffer_valid(DELAY-n);
 		end loop;
 	end process;
 	
---
+++ 1.testbenchAndDoScripts/delayChannel.tb.vhd
@@ -0,0 +1,100 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_textio.all;
+use std.textio.all;
+use work.customTypes.all;
+
+entity tb_delayChannel is 
+end tb_delayChannel;
+
+architecture testbench of tb_delayChannel is
+	
+	signal clk : std_logic := '0';
+	signal reset : std_logic;
+	signal finished    : boolean := false;
+    signal currenttime : time    := 0 ns;	
+	constant CLK_PERIOD : time := 10 ns;	
+	
+	signal d_in : std_logic_vector(31 downto 0);
+	signal d_out : vectorArray_t(0 to 3)(31 downto 0);
+	
+	signal p_valid, n_ready,
+			ready : std_logic;
+	signal valid : bitArray_t(0 to 3);
+	
+begin
+	
+	-- run simulation
+	sim : process
+		--simulation reset
+		procedure resetSim is
+			begin
+				reset <= '1';
+				p_valid <= '0';
+				n_ready <= '0';
+				d_in <= (others => '0');
+				wait until rising_edge(clk);
+				wait for(CLK_PERIOD / 4);
+				reset<='0';
+		end procedure resetSim;			
+		--waiting procedures
+		procedure waitPeriod(constant i : in real) is
+		begin
+			wait for i * CLK_PERIOD;
+		end procedure;		
+		procedure waitPeriod(constant i : in integer) is
+		begin
+			wait for i * CLK_PERIOD;
+		end procedure;	
+		--text output procedures
+		variable console_out : line;
+		procedure newline is
+		begin
+			console_out := new string'("");
+			writeline(output, console_out);
+		end procedure newline;
+		procedure print(msg : in string) is
+		begin
+			console_out := new string'(msg);
+			writeline(output, console_out);
+		end procedure print;
+		-- finished procedures
+	begin
+	resetSim;
+	if(not finished)then 
+		p_valid <= '1';
+		n_ready <= '1';
+		d_in <= X"00000007";
+		waitPeriod(1);
+		d_in <= X"00000006";
+		waitPeriod(1);
+		d_in <= X"00000005";
+		waitPeriod(1);
+		d_in <= X"00000004";
+		waitPeriod(1);
+		d_in <= X"00000003";
+		waitPeriod(1);
+		p_valid <= '0';
+		waitPeriod(3);
+		
+	end if;
+	finished <= true;
+	end process;
+	
+	-- instantiate design under test
+	DUT : entity work.delayChannel(generique) generic map(32, 3)
+			port map(clk, reset, d_in, d_out, p_valid, n_ready, ready, valid);
+				
+	-- ticks the clock
+	clock : process
+    begin
+        if (finished) then
+            wait;
+        else
+            clk <= not clk;
+            wait for CLK_PERIOD / 2;
+            currenttime <= currenttime + CLK_PERIOD / 2;
+        end if;
+    end process clock;
+      
+end testbench;
