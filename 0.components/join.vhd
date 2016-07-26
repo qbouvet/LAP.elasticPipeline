eaddaffe75f065d5e3d8e9945d7d2e8f0ba9aed6
sgPepper <quentin.bouvet@hotmail.fr>
2016-07-26 19:14:45 +0200

    added decent version of join3

--- 0.components/join.vhd
+++ 0.components/join.vhd
@@ -22,3 +22,43 @@ begin
 	ready1 <= (not p_valid1) or (p_valid0 and p_valid1 and n_ready); 
 
 end lazy;
+
+
+
+
+
+
+
+
+--------------------------------------------------------------  join3
+---------------------------------------------------------------------
+-- and adapted version of the join, based on the same logic 
+-- equations, to join the control signals from 3 inputs
+---------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use work.customTypes.all;
+
+entity join3 is
+port(	pValidArray : in bitArray_t(2 downto 0);
+		n_ready : in std_logic;
+		valid : out std_logic;
+		readyArray : out bitArray_t(2 downto 0));
+end join3;
+
+architecture lazy of join3 is
+		-- internal valud for readybility
+	signal allPValid : std_logic;
+begin
+
+	valid <= pValidArray(0) and pValidArray(1) and pValidArray(2);
+	
+	allPValid <= (pValidArray(0) and pValidArray(1) and (pValidArray2));
+	
+	readyArray(0) <= (not pValidArray(0)) or allPValid
+	readyArray(1) <= (not pValidArray(1)) or allPValid
+	readyArray(2) <= (not pValidArray(2)) or allPValid
+	
+	
+end lazy;
+		
