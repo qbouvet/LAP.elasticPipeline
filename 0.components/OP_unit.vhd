c3865c73f06af5be2d5a1121f513eb5ca469715c
sgPepper <quentin.bouvet@hotmail.fr>
2016-07-27 12:30:05 +0200

    removed old non-elastic architecture, non compatible with new entity

--- 0.components/OP_unit.vhd
+++ 0.components/OP_unit.vhd
@@ -110,37 +110,3 @@ begin
 			(selector_opReady, readyArray(0)) <= selector_ready_array;
 	
 end elastic;
-
-------------------------------------------------------------------------
--- vanilla implementation without elastic control signals
-------------------------------------------------------------------------
-architecture vanilla of OP_unit is
-	signal imm_ext, addi_res, res_op0, res_op1 : std_logic_vector (31 downto 0);
-begin
-
--- choose the output according to the op/opx/oc
-	process(addi_res, res_op0, res_op1, op, opx, oc)
-	begin
-		case op(5) is 
-			-- immediate instruction (addi)
-			when '1' => res <= addi_res;
-			-- other instructions
-			when '0' => 
-				case opx(1) is	
-					when '0' => res <= res_op0;
-					when '1' => res <= res_op1;
-					when others => res <= (others => 'U');
-				end case;
-			when others => res <= (others => 'U');
-		end case;
-	end process;
-	
--- instantiate operations
-	addi : entity work.adder port map(imm_ext, argA, addi_res, open);
-	op0 : entity work.sample_op_0 port map (argA, argB, res_op0);
-	op1 : entity work.adder port map(argA, X"00000001", res_op1, open);
--- extend the immediate argument to a 32 bits signal
-	imm_ext <= X"0000" & immArg;
-end vanilla;
-
-
