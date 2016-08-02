746a7575ace6c1a1a1bef564340eb0f18328e78a
sgPepper <quentin.bouvet@hotmail.fr>
2016-08-02 16:42:12 +0200

    narrowing down bug

--- 0.components/OPunit.vhd
+++ 0.components/OPunit.vhd
@@ -74,53 +74,235 @@ end OPunit;
 -- version with simple elastic control signals implementation
 ------------------------------------------------------------------------
 architecture elastic of OPunit is
-	-- results
-	signal res0, res1 : std_logic_vector (31 downto 0);
-	--fork control signals
-	signal forkA_nReadyArray, forkA_validArray : bitArray_t(1 downto 0);
-	signal forkA_ready : std_logic;
+	signal fork_nReadyArray, fork_validArray : bitArray_t(1 downto 0);	-- (res1, res0), (op1, op0)
+	signal res0, res1 : std_logic_vector(31 downto 0);
+	signal selector_readyArray : bitArray_t(2 downto 0); 				-- (res1, res0, oc)
+	signal op0_readyArray, op1_readyArray : bitArray_t(1 downto 0);		-- (argA, argI), (argB, argA)
 	signal op0_valid, op1_valid : std_logic;
-	-- contient les bits n_ready pour op1, op0; oc is done via the entity's signal
-	signal selector_opReady : bitArray_t(1 downto 0);
-	-- temporary signals since I cant aggregate outputs directly in entities' port maps
-	signal addi_ready_array, op1_ready_array : bitArray_t(1 downto 0);
-	signal selector_ready_array : bitArray_t(2 downto 0);	
 begin
 	
 	-- fork for argument A
 	forkA : entity work.forkN generic map(2)
 			port map (	clk, reset, 
 						pValidArray(2), 					-- p_valid
-						forkA_nReadyArray, 					-- nReadyArray
-						forkA_validArray,					-- validArray
-						readyArray(2));						-- ready
+						fork_nReadyArray, 					-- nReadyArray
+						readyArray(2),						-- ready
+						fork_validArray);					-- validArray	
 	
 	addi : entity work.op0
 			port map (	argA, argI, 
 						res0, 
-						forkA_validArray(0) & pValidArray(1),--pValidArray
-						selector_opReady(0), 				-- nReady
-						addi_ready_array, 					-- readyArray
-						op0_valid);							-- valid
-	(forkA_nReadyArray(0), readyArray(1)) <= addi_ready_array;
+						(fork_validArray(0), pValidArray(1)),	--pValidArray
+						selector_readyArray(1), 				-- nReady
+						op0_readyArray, 						-- readyArray
+						op0_valid);								-- valid
+	(fork_nReadyArray(0), readyArray(1)) <= op0_readyArray;
 			
 	sampleOp1 : entity work.op1
 			port map (	argA, argB, 
 						res1, 
-						forkA_validArray(1) & pValidArray(3),--p_valid_array
-						selector_opReady(1), 				-- n_ready
-						op1_ready_array, 					-- readyArray
-						op1_valid);							-- valid
-	(forkA_nReadyArray(1), readyArray(3)) <= op1_ready_array;
+						(fork_validArray(1), pValidArray(3)),	--p_valid_array
+						selector_readyArray(2), 				-- n_ready
+						op1_readyArray, 						-- readyArray
+						op1_valid);								-- valid
+	(fork_nReadyArray(1), readyArray(3)) <= op1_readyArray;
 	
 	--prends dans l'ordre les control signals de : (2)res1, (1)res0, (0)oc				
 	selector : entity work.selectorBlock
 			port map (	res1, res0, oc, 
 						res, 
-						op1_valid & op0_valid & pValidArray(0),	-- pValidArray
+						(op1_valid, op0_valid, pValidArray(0)),	-- pValidArray
 						nReady, 								-- nReady
-						selector_ready_array, 					-- readyArray
+						selector_readyArray, 					-- readyArray	-- (op1, op0, oc)
 						valid);									-- valid
-	(selector_opReady, readyArray(0)) <= selector_ready_array;
+	readyArray(0) <= selector_readyArray(0);
 	
 end elastic;
+
+
+
+
+
+
+
+
+
+
+
+------------------------------------------------------------------------
+-- version with fewer blocks in it for debug purpose - exceeds iterations limits
+------------------------------------------------------------------------
+architecture debug3 of OPunit is
+
+	signal fork_nReadyArray, fork_validArray : bitArray_t(1 downto 0);	-- (res1, res0), (op1, op0)
+	signal res0, res1 : std_logic_vector(31 downto 0);
+	signal selector_readyArray : bitArray_t(2 downto 0); 				-- (res1, res0, oc)
+	signal op0_readyArray, op1_readyArray : bitArray_t(1 downto 0);		-- (argA, argI), (argB, argA)
+	signal op0_valid, op1_valid : std_logic;
+	
+begin
+
+	res1 <= argA;
+	op1_valid <= fork_validArray(1);
+	
+	forkArgA : entity work.forkN generic map(2)
+			port map(	clk, reset,
+						pValidArray(2),
+						fork_nReadyArray,
+						readyArray(2),
+						fork_validArray);
+	fork_nReadyArray <= ( selector_readyArray(2), op0_readyArray(1));
+	
+	addi : entity work.op0
+			port map (	argA, argI, 
+						res0, 
+						(fork_validArray(0), pValidArray(1)),	--pValidArray
+						selector_readyArray(1), 				-- nReady
+						op0_readyArray, 						-- readyArray
+						op0_valid);								-- valid
+	readyArray(1) <= op0_readyArray(0);
+	
+	--prends dans l'ordre les control signals de : (2)res1, (1)res0, (0)oc				
+	selector : entity work.selectorBlock
+			port map (	res1, res0, oc, 
+						res, 
+						(op1_valid, op0_valid, pValidArray(0)),	-- pValidArray
+						nReady, 									-- nReady
+						selector_readyArray, 						-- readyArray	-- (op1, op0, oc)
+						valid);										-- valid
+	readyArray(3) <= '0'; --not used now
+	readyArray(0) <= selector_readyArray(0);
+	
+end debug3;
+
+
+
+
+------------------------------------------------------------------------
+-- version without operations in it for debug purpose - doesn't exceed iteration limit
+------------------------------------------------------------------------
+architecture debug2 of OPunit is
+	-- results
+	signal res0, res1 : std_logic_vector (31 downto 0);
+	--fork control signals
+	--signal forkA_nReadyArray, forkA_validArray : bitArray_t(1 downto 0);
+	--signal forkA_ready : std_logic;
+	signal op0_valid, op1_valid : std_logic;
+	-- temporary signals since I cant aggregate outputs directly in entities' port maps
+	signal addi_ready_array, op1_ready_array : bitArray_t(1 downto 0);
+	signal selector_readyArray : bitArray_t(2 downto 0);	
+begin
+
+	res1 <= argB;
+	op1_valid <= pValidArray(3);
+	
+	addi : entity work.op0
+			port map (	argA, argI, 
+						res0, 
+						(pValidArray(2), pValidArray(1)),	--pValidArray
+						selector_readyArray(1), 				-- nReady
+						addi_ready_array, 						-- readyArray
+						op0_valid);								-- valid
+	(readyArray(2), readyArray(1)) <= addi_ready_array;
+	
+	--prends dans l'ordre les control signals de : (2)res1, (1)res0, (0)oc				
+	selector : entity work.selectorBlock
+			port map (	res1, res0, oc, 
+						res, 
+						(op1_valid, op0_valid, pValidArray(0)),	-- pValidArray
+						nReady, 									-- nReady
+						selector_readyArray, 						-- readyArray	-- (op1, op0, oc)
+						valid);										-- valid
+	readyArray(3) <= selector_readyArray(2);
+	readyArray(0) <= selector_readyArray(0);
+	
+end debug2;
+
+
+
+
+------------------------------------------------------------------------
+-- version without operations in it for debug purpose - doesn't exceed iteration limit
+------------------------------------------------------------------------
+architecture debug1 of OPunit is
+	-- results
+	signal res0, res1 : std_logic_vector (31 downto 0);
+	--fork control signals
+	--signal forkA_nReadyArray, forkA_validArray : bitArray_t(1 downto 0);
+	--signal forkA_ready : std_logic;
+	signal op0_valid, op1_valid : std_logic;
+	-- temporary signals since I cant aggregate outputs directly in entities' port maps
+	--signal addi_ready_array, op1_ready_array : bitArray_t(1 downto 0);
+	signal selector_readyArray : bitArray_t(2 downto 0);	
+begin
+
+	res0 <= argA;
+	res1 <= argB;
+	op0_valid <= pValidArray(2);
+	op1_valid <= pValidArray(3);
+	
+	--prends dans l'ordre les control signals de : (2)res1, (1)res0, (0)oc				
+	selector : entity work.selectorBlock
+			port map (	res1, res0, oc, 
+						res, 
+						(op1_valid, op0_valid, pValidArray(0)),	-- pValidArray
+						nReady, 									-- nReady
+						selector_readyArray, 						-- readyArray	-- (op1, op0, oc)
+						valid);										-- valid
+	(readyArray(3), readyArray(2), readyArray(0)) <= selector_readyArray;
+	readyArray(1) <= '1';
+	
+end debug1;
+
+
+
+
+
+------------------------------------------------------------------------
+-- for debug purpose - in the end, exactly the same thing as 'elastic' architecture
+------------------------------------------------------------------------
+architecture elastic2 of OPunit is
+	
+	signal fork_nReadyArray, fork_validArray : bitArray_t(1 downto 0);	-- (res1, res0), (argB, argA)
+	signal res0, res1 : std_logic_vector(31 downto 0);
+	signal selector_readyArray : bitArray_t(2 downto 0); 				-- (res1, res0, oc)
+	signal op0_readyArray, op1_readyArray : bitArray_t(1 downto 0);		-- (argA, argI), (argB, argA)
+	signal op0_valid, op1_valid : std_logic;
+	
+begin
+	
+	forkArgA : entity work.forkN generic map (2)
+			port map (	clk, reset,
+						pValidArray(2),
+						fork_nReadyArray,
+						readyArray(2),
+						fork_validArray);
+						
+	addi : entity work.op0 
+			port map(	argA, argI,
+						res0, 
+						(fork_validArray(0), pValidArray(1)),
+						selector_readyArray(1),
+						op0_readyArray,	--(argA, argI)
+						op0_valid);
+	(fork_nReadyArray(0), readyArray(1)) <= op0_readyArray;
+	
+	sample_op1 : entity work.op1 
+			port map(	argB, argA,
+						res1, 
+						(fork_validArray(1), pValidArray(3)),
+						selector_readyArray(2),
+						op1_readyArray,
+						op1_valid);
+	(fork_nReadyArray(1), readyArray(3)) <= op1_readyArray;
+	
+	selector : entity work.selectorBlock
+			port map( 	res1, res0, oc,
+						res, 
+						(op1_valid, op0_valid, pValidArray(0)),
+						nReady,
+						selector_readyArray,
+						valid);
+	readyArray(0) <= selector_readyArray(0);
+	
+end elastic2;
--- 1.testbenchAndDoScripts/OPunit.tb.vhd
+++ 1.testbenchAndDoScripts/OPunit.tb.vhd
@@ -8,7 +8,11 @@ use work.customTypes.all;
 entity TB_OPunit is 
 end TB_OPunit;
 
-architecture testbench of TB_OPunit is
+
+------------------------------------------------------------------------
+-- tests the OPunit itself
+------------------------------------------------------------------------
+architecture OPunit of TB_OPunit is
 
 	signal finished    : boolean := false;
     signal currenttime : time    := 0 ns;	
@@ -25,26 +29,6 @@ architecture testbench of TB_OPunit is
 begin
 	
 	sim : process
-		procedure resetSim is
-		begin
-			reset <= '1';
-			oc <= (others => '0');
-			argI <= (others => '0');
-			argA <= (others => '0');
-			argB <= (others => '0');
-			pValidArray <= (others => '0');
-			nReady <= '0';
-			wait until rising_edge(CLK);
-			wait for CLK_PERIOD / 4;
-			reset <= '0';
-		end procedure;			
-		--waiting procedures
-		procedure waitPeriod(constant i : in real) is
-		begin	wait for i * CLK_PERIOD;
-		end procedure;		
-		procedure waitPeriod(constant i : in integer) is
-		begin	wait for i * CLK_PERIOD;
-		end procedure;	
 		--text output procedures
 		variable console_out : line;
 		procedure newline is
@@ -55,14 +39,35 @@ begin
 		begin	console_out := new string'(msg);
 				writeline(output, console_out);
 		end procedure print;
+		procedure resetSim is
+		begin
+			print("reset");
+			reset <= '1';
+			oc <= (others => '0');
+			argI <= (others => '0');
+			argA <= (others => '0');
+			argB <= (others => '0');
+			pValidArray <= (others => '0');
+			nReady <= '0';
+			wait until rising_edge(CLK);
+			wait for CLK_PERIOD / 4;
+			reset <= '0';
+			print("reset done");
+		end procedure;			
+		--waiting procedures
+		procedure waitPeriod(constant i : in real) is
+		begin	wait for i * CLK_PERIOD;
+		end procedure;		
+		procedure waitPeriod(constant i : in integer) is
+		begin	wait for i * CLK_PERIOD;
+		end procedure;	
 		-- finished procedures
 	begin
-	
 		resetSim;
 		if(not finished)then
 		
 			newline;print("simulation started");
-			newline;print(" basic tests");
+			newline;print("basic tests");
 			-- addi test
 			oc <= X"00000" & "000000100000"; -- op0-addi		
 			argA <= X"00000001";
@@ -73,76 +78,23 @@ begin
 			assert res=X"00000003" report "addi fails" severity error;
 			
 			waitPeriod(0.5);
-			resetSim;
+			--insert here
 			
-			--op1 test
-			oc <= X"00000" & "000001000000"; -- op1
-			argB <= X"00000001";
-			argA <= X"00000002";
-			nReady <= '1';
-			pValidArray <= (others => '1');
-			waitPeriod(1);
-			assert res=X"00000006" report "op1 fails" severity error;
-						
-			waitPeriod(0.5);
-			resetSim;
-			
-			newLine; print("control signals tests");
-			oc <= X"00000" & "000000100000"; -- op0-addi		
-			argA <= X"00000001";
-			argI <= X"00000002";
-			nReady <= '1';
-			pValidArray <= "1110";
-			waitPeriod(1);
-			assert valid='0' report "must stay invalid until all data is available" severity error;
-			assert readyArray="0001" report "ready signals array must switch to 0 on ready buffers when not all data is available, so that they keep the informations" severity error;
-			pValidArray <= "1101";
-			waitPeriod(1);
-			assert valid='0' report "must stay invalid until all data is available (2)" severity error;
-			assert readyArray="0010" report "ready signals array must switch to 0 on ready buffers when not all data is available, so that they keep the informations (2)" severity error;
-			pValidArray <= "1011";
-			waitPeriod(1);
-			assert valid='0' report "must stay invalid until all data is available (3)" severity error;
-			assert readyArray="0100" report "ready signals array must switch to 0 on ready buffers when not all data is available, so that they keep the informations (3)" severity error;
-			pValidArray <= "0111";
-			waitPeriod(1);
-			assert valid='0' report "must stay invalid until all data is available (4)" severity error;
-			assert readyArray="1000" report "ready signals array must switch to 0 on ready buffers when not all data is available, so that they keep the informations (4)" severity error;
-			pValidArray <= "0000";
-			waitPeriod(1);
-			assert valid='0' report "must stay invalid until all data is available (5)" severity error;
-			assert readyArray="1111" report "ready signals array must switch to 0 on ready buffers when not all data is available, so that they keep the informations (5)" severity error;
-			pValidArray <= "1111";			
-			nReady <= '0';
-			waitPeriod(1);
-			assert valid='0' report "must stay invalid until all data is available (6)" severity error;
-			assert readyArray="0000" report "ready signals array must switch to 0 when the next component isn't ready" severity error;
-			nReady <= '1';
-			waitPeriod(0.5);
-			assert valid = '1' report "should be valid now" severity error;
-			assert readyArray = "1111" report "should accept data now" severity error;
-			wait until rising_edge(CLK);
-			pValidArray <= "0000";
-			waitPeriod(0.25);
-			assert valid='0' report "no more data, can't be valid" severity error;
-			assert readyArray = "1111" report "next component still ready, no data incommind, should be ready" severity error;
-			
-			
-			waitPeriod(1);
-			newline;print("simluation finished");
-			finished <= true;
 		end if;		
 		
+		newline;print("simluation finished");
+		finished <= true;
 	end process sim;
 
 	-- DUT instance
-	opu : entity work.OPunit port map(	clk, reset, 
-										argB, argA, argI, oc, 
-										res, 
-										pValidArray,
-										nReady,
-										valid, 
-										readyArray);
+	opu : entity work.OPunit(debug3)
+		port map(	clk, reset, 
+					argB, argA, argI, oc, 
+					res, 
+					pValidArray,
+					nReady,
+					valid, 
+					readyArray);
 	
 	-- ticks the clock
 	clock : process
@@ -156,7 +108,159 @@ begin
         end if;
     end process clock;
     
-end testbench;
+end OPunit;
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+------------------------------------------------------------------------
+-- tests the selector block from the OPunit
+------------------------------------------------------------------------
+architecture selectorBlock of TB_OPunit is
+	
+	signal clk : std_logic := '0';
+	signal reset : std_logic;
+	signal finished    : boolean := false;
+    signal currenttime : time    := 0 ns;	
+	constant CLK_PERIOD : time := 10 ns;	
+	
+	signal res1, res0, oc, res : std_logic_vector(31 downto 0);
+	signal pValidArray,readyArray : bitArray_t(2 downto 0);
+	signal nReady, valid : std_logic;
+	
+begin
+	
+	-- run simulation
+	sim : process
+		-- reset procedure
+		procedure resetSim is
+			begin
+				reset <= '1';
+				pValidArray <= (others => '0');
+				nReady <= '0';
+				res1 <= (others => '0');
+				res0 <= (others => '0');
+				oc <= (others => '0');
+				wait until rising_edge(clk);
+				wait for(CLK_PERIOD / 4);
+				reset<='0';
+		end procedure resetSim;			
+		--waiting procedures
+		procedure waitPeriod(constant i : in real) is
+		begin	wait for i * CLK_PERIOD;
+		end procedure;		
+		procedure waitPeriod(constant i : in integer) is
+		begin	wait for i * CLK_PERIOD;
+		end procedure;	
+		--text output procedures
+		variable console_out : line;
+		procedure newline is
+		begin	console_out := new string'("");
+					writeline(output, console_out);
+		end procedure newline;
+		procedure print(msg : in string) is
+		begin	console_out := new string'(msg);
+					writeline(output, console_out);
+		end procedure print;
+		-- finished procedures
+	begin
+	resetSim;
+	if(not finished)then 
+		
+		print("quick test for the data part");
+		res0 <= X"00000001";
+		res1 <= X"00000002";
+		waitPeriod(1);
+		oc <= "00000000000000000000000000100000";
+		waitPeriod(1);
+		assert res=res0;
+		oc <= "00000000000000000000000001000000";
+		waitPeriod(1);
+		assert res=res1;
+		
+		resetSim;
+		res0 <= X"00000001";
+		res1 <= X"00000002";
+		oc <= "00000000000000000000000000100000";
+		
+		print("control signals tests");
+		-- NB 'valid0 depends only on wether the previous data is valid
+		-- 'ready(x)' depends on wether nReady='1' but also on wether all other previous data are valid
+		-- nReady = '0'
+		pValidArray <= "001";
+		waitPeriod(1);
+		assert valid = '0';
+		assert readyArray = "110";
+		pValidArray <= "010";	
+		waitPeriod(1);	
+		assert valid = '0';
+		assert readyArray = "101";
+		pValidArray <= "100";
+		waitPeriod(1);
+		assert valid = '0';
+		assert readyArray = "011";
+		pValidArray <= "110";
+		waitPeriod(1);
+		assert valid = '0';
+		assert readyArray = "001";
+		pValidArray <= "111";
+		waitPeriod(1);		
+		assert valid = '1';
+		-- nReady = '1'
+		nReady <= '1';
+		pValidArray <= "001";
+		waitPeriod(1);
+		assert valid = '0';
+		pValidArray <= "010";	
+		waitPeriod(1);	
+		assert valid = '0';
+		pValidArray <= "100";
+		waitPeriod(1);
+		assert valid = '0';
+		pValidArray <= "110";
+		waitPeriod(1);
+		assert valid = '0';
+		pValidArray <= "111";
+		waitPeriod(1);
+		assert valid = '1';
+		assert readyArray = "111";
+		
+		newline;print("simulation finished");
+		
+	end if;
+	finished <= true;
+	end process;
+	
+	-- instantiate design under test
+	DUT : entity work.selectorBlock 
+			port map(	res1, res0, oc,
+						res,
+						pValidArray,
+						nReady, 
+						readyArray,
+						valid);
+	
+	-- ticks the clock
+	clock : process
+    begin
+        if (finished) then
+            wait;
+        else
+            clk <= not clk;
+            wait for CLK_PERIOD / 2;
+            currenttime <= currenttime + CLK_PERIOD / 2;
+        end if;
+    end process clock;
+      
+end selectorBlock;
 	
 	
 	
@@ -168,6 +272,249 @@ end testbench;
 	
 	
 	
+	
+	
+	
+	
+
+
+------------------------------------------------------------------------
+-- tests op1 (addi)
+------------------------------------------------------------------------
+architecture op1 of TB_OPunit is
+	
+	signal clk : std_logic := '0';
+	signal reset : std_logic;
+	signal finished    : boolean := false;
+    signal currenttime : time    := 0 ns;	
+	constant CLK_PERIOD : time := 10 ns;	
+	
+	signal a, b, res : std_logic_vector(31 downto 0);
+	signal pValidArray, readyArray : bitArray_t(1 downto 0);
+	signal nReady, valid : std_logic;
+	
+begin
+	
+	-- run simulation
+	sim : process
+		-- reset procedure
+		procedure resetSim is
+			begin
+				reset <= '1';
+				a <= (others => '0');
+				b <= (others => '0');
+				pValidArray  <= (others => '0');
+				nReady <= '0';
+				wait until rising_edge(clk);
+				wait for(CLK_PERIOD / 4);
+				reset<='0';
+		end procedure resetSim;			
+		--waiting procedures
+		procedure waitPeriod(constant i : in real) is
+		begin	wait for i * CLK_PERIOD;
+		end procedure;		
+		procedure waitPeriod(constant i : in integer) is
+		begin	wait for i * CLK_PERIOD;
+		end procedure;	
+		--text output procedures
+		variable console_out : line;
+		procedure newline is
+		begin	console_out := new string'("");
+					writeline(output, console_out);
+		end procedure newline;
+		procedure print(msg : in string) is
+		begin	console_out := new string'(msg);
+					writeline(output, console_out);
+		end procedure print;
+		-- finished procedures
+	begin
+	resetSim;
+	if(not finished)then 
+		
+		a <= X"00000001";
+		b <= X"00000001";	
+		
+		pValidArray <= "00";
+		waitPeriod(1);
+		assert readyArray="11";
+		pValidArray <= "01";
+		waitPeriod(1);
+		assert readyArray="10";
+		pValidArray <= "10";
+		waitPeriod(1);
+		assert readyArray="01";
+		pValidArray <= "11";
+		waitPeriod(1);
+		assert readyArray="00";
+		
+		nReady <= '1';
+		pValidArray <= "00";
+		waitPeriod(1);
+		assert readyArray="11";
+		pValidArray <= "01";
+		waitPeriod(1);
+		assert readyArray="10";
+		pValidArray <= "10";
+		waitPeriod(1);
+		assert readyArray="01";
+		pValidArray <= "11";
+		waitPeriod(1);
+		assert readyArray="11";
+		
+		a <= X"00000002";
+		b <= X"00000001";
+		waitPeriod(1);
+		a <= X"00000002";
+		b <= X"00000003";
+		waitPeriod(1);
+		
+	end if;
+	finished <= true;
+	end process;
+	
+	-- instantiate design under test
+	DUT : entity work.op1
+			port map( 	a, b, 
+						res, 
+						pValidArray,
+						nReady,
+						readyArray,
+						valid);
+	
+	-- ticks the clock
+	clock : process
+    begin
+        if (finished) then
+            wait;
+        else
+            clk <= not clk;
+            wait for CLK_PERIOD / 2;
+            currenttime <= currenttime + CLK_PERIOD / 2;
+        end if;
+    end process clock;
+      
+end op1;
+
+
+
+
+
+
+
+------------------------------------------------------------------------
+-- tests op0 (addi)
+------------------------------------------------------------------------
+architecture addi of TB_OPunit is
+	signal clk : std_logic := '0';
+	signal reset : std_logic;
+	signal finished    : boolean := false;
+    signal currenttime : time    := 0 ns;	
+	constant CLK_PERIOD : time := 10 ns;	
+	
+	signal a, b, res : std_logic_vector(31 downto 0);
+	signal pValidArray, readyArray : bitArray_t(1 downto 0);
+	signal nReady, valid : std_logic;
+begin
+	
+	-- run simulation
+	sim : process
+		-- reset procedure
+		procedure resetSim is
+			begin
+				reset <= '1';
+				a <= (others => '0');
+				b <= (others => '0');
+				pValidArray  <= (others => '0');
+				nReady <= '0';
+				wait until rising_edge(clk);
+				wait for(CLK_PERIOD / 4);
+				reset<='0';
+		end procedure resetSim;			
+		--waiting procedures
+		procedure waitPeriod(constant i : in real) is
+		begin	wait for i * CLK_PERIOD;
+		end procedure;		
+		procedure waitPeriod(constant i : in integer) is
+		begin	wait for i * CLK_PERIOD;
+		end procedure;	
+		--text output procedures
+		variable console_out : line;
+		procedure newline is
+		begin	console_out := new string'("");
+					writeline(output, console_out);
+		end procedure newline;
+		procedure print(msg : in string) is
+		begin	console_out := new string'(msg);
+					writeline(output, console_out);
+		end procedure print;
+		-- finished procedures
+	begin
+	resetSim;
+	if(not finished)then 
+		
+		a <= X"00000001";
+		b <= X"00000001";	
+		
+		pValidArray <= "00";
+		waitPeriod(1);
+		assert readyArray="11";
+		pValidArray <= "01";
+		waitPeriod(1);
+		assert readyArray="10";
+		pValidArray <= "10";
+		waitPeriod(1);
+		assert readyArray="01";
+		pValidArray <= "11";
+		waitPeriod(1);
+		assert readyArray="00";
+		
+		nReady <= '1';
+		pValidArray <= "00";
+		waitPeriod(1);
+		assert readyArray="11";
+		pValidArray <= "01";
+		waitPeriod(1);
+		assert readyArray="10";
+		pValidArray <= "10";
+		waitPeriod(1);
+		assert readyArray="01";
+		pValidArray <= "11";
+		waitPeriod(1);
+		assert readyArray="11";
+		
+		a <= X"00000002";
+		b <= X"00000001";
+		waitPeriod(1);
+		a <= X"00000002";
+		b <= X"00000003";
+		waitPeriod(1);
+			
+	end if;
+	finished <= true;
+	end process;
+	
+	-- instantiate design under test
+	DUT : entity work.op0
+			port map( 	a, b, 
+						res, 
+						pValidArray,
+						nReady,
+						readyArray,
+						valid);
+	
+	-- ticks the clock
+	clock : process
+    begin
+        if (finished) then
+            wait;
+        else
+            clk <= not clk;
+            wait for CLK_PERIOD / 2;
+            currenttime <= currenttime + CLK_PERIOD / 2;
+        end if;
+    end process clock;
+      
+end addi;
 	
 	
 	
