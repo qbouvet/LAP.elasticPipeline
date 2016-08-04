3e36acd6a261bd599873cddc4c9727fbddc7488b
sgPepper <quentin.bouvet@hotmail.fr>
2016-08-04 16:08:16 +0200

    new data providing process + redone testbench for the new IFD

--- 1.testbenchAndDoScripts/instructionFetcherDecoder.tb.vhd
+++ 1.testbenchAndDoScripts/instructionFetcherDecoder.tb.vhd
@@ -3,47 +3,30 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use ieee.std_logic_textio.all;
 use std.textio.all;
+use work.customTypes.all;
 
-entity TB_IFD is
-end TB_IFD;
+entity TB_instructionFetcherDecoder is
+end TB_instructionFetcherDecoder;
 
-architecture testbench of TB_IFD is
+architecture testbench of TB_instructionFetcherDecoder is
 
 	signal finished    : boolean := false;
     signal currenttime : time    := 0 ns;	
-	signal reset : std_logic;
+	signal reset : std_logic := '1';
 	signal clk : std_logic := '0';
 	
 	constant CLK_PERIOD : time := 10 ns;
 
-	component IFD is port(
-		reset, clk : in std_logic;
+	signal instr, adrA, adrB, adrW, argI, oc : std_logic_vector(31 downto 0);
+	signal instrValid, ifdReady : std_logic;
+	signal nReadyArray, validArray : bitArray_t(4 downto 0);	
 	
-		rddata : in std_logic_vector(31 downto 0);
-		rd_addr : out std_logic_vector(31 downto 0);
-		
-		read_instr : out std_logic;
-		instr : out std_logic_vector(31 downto 0);
-		
-		rf_wren : out std_logic;
-		adrA, adrB, wr_adr : out std_logic_vector(31 downto 0);
-		
-		op,opx : out std_logic_vector(5 downto 0); --may be replaced by oc
-		oc : out std_logic_vector(31 downto 0)); --for extension
-	end component;
-	signal rddata : std_logic_vector(31 downto 0);
-	signal rd_addr : std_logic_vector(31 downto 0);
-	signal instr : std_logic_vector(31 downto 0);
-	signal read_instr : std_logic := '0';
-	signal rf_wren : std_logic;
-	signal op, opx : std_logic_vector(5 downto 0);
-	signal oc, adrA, adrB, wr_adr : std_logic_vector(31 downto 0);
+	signal currentHeldInstruction : std_logic_vector(31 downto 0); 
 	
 begin
 
 	-- design under test
-	DUT : IFD port map (
-		reset, clk, rddata, rd_addr, read_instr, instr, rf_wren, adrA, adrB, wr_adr, op, opx, oc);
+
 
 	-- ticks the clock
 	clock : process
@@ -57,11 +40,12 @@ begin
         end if;
     end process clock;
 	
-	--provide the data
+	-- reads the next line, verifies it's not a comment, then holds it until the next rising clock where ifdReady='1' 
 	data_prvd : process
-		file instr_f : text is in "instructionFetcherDecoder.instruction.txt";
+		file instr_f : text is in "/home/quentin/Desktop/LAP/00.ElasticPipeline/1.testbenchAndDoScripts/instructionFetcherDecoder.instruction.txt";
 		variable line_in : line;
 		variable WORD : std_logic_vector(31 downto 0);
+		variable readInstruction : boolean := false;
 	-- text output procedures
 		variable console_out : line;
 		procedure newline is
@@ -74,35 +58,39 @@ begin
 			console_out := new string'(msg);
 			writeline(output, console_out);
 		end procedure print;
-	begin
-		if(endfile(instr_f)) then	
-		wait for 5 * CLK_PERIOD;
-			finished <= true;
-			wait;
-		else				
-			line_in := new string'("");
-			readline(instr_f, line_in);
-			if(line_in'length /= 0 )then	
-				if(line_in(1) = 'x') then
-					finished <= true;
-				elsif(line_in(1) /= '#' and line_in(1) /= '|') then
-					wait until rising_edge(read_instr);
-					read(line_in, WORD);
-					rddata <= WORD;
-					wait until read_instr = '0';
+	-- text read procedures
+		procedure readNextInstruction is
+		begin		
+			readInstruction := false;
+			while not readInstruction loop													-- while we didn't read an actual instruction
+				if(endfile(instr_f)) then														-- if end of file
+					instrValid <= '0';																-- set the finished and instrValid signals
+					finished <= true;				
+					wait;
+				else																			-- else
+					line_in := new string'("");														-- read next line_in	
+					readline(instr_f, line_in);														-- if it's not a comment
+					if(line_in'length /= 0 and line_in(1) /= '#' and line_in(1) /= '|')then				-- set instruction and readInstruction boolean
+						instrValid <= '1';
+						read(line_in, WORD);
+						instr <= WORD;
+						readInstruction := true;
+						instrValid <= '1';
+					end if;
 				end if;
-			end if;
+			end loop;
+		end procedure readNextInstruction;
+	--end procedures
+	begin
+		if reset = '1' then
+			instr <= (others => '0');
+			instrValid <= '0';
+			wait until reset = '0';
+		else		
+			readNextInstruction;
+			wait until rising_edge(clk) and ifdReady='1';
 		end if;
 end process data_prvd;
-	
---check instr is correctly outputteds	
-	check : process 
-	begin
-		wait until rising_edge(read_instr);
-		wait until rising_edge(clk);
-		wait for CLK_PERIOD / 10;  ----------------------------------------------------------------  problem here ?
-		assert instr = rddata report "instr and rddata mismatch" severity error;
-	end process;
 	
 -- run simulation
 	sim : process is	
@@ -113,8 +101,23 @@ end process data_prvd;
 			wait for 3 * CLK_PERIOD / 4;
 			reset <= '0';
 		end procedure reset_sim;
+	--waiting procedures
+		procedure waitPeriod is
+		begin	wait for CLK_PERIOD;
+		end procedure;	
 	begin
 		reset_sim;
+		
+		ifdReady <= '0';
+		waitPeriod;
+		
+		waitPeriod;
+		
+		ifdReady <= '1';
+		waitPeriod;
+		waitPeriod;
+		
 		wait;
+		
 	end process;
 end testbench;
