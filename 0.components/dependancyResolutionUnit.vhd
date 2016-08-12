91a9f3c26e7107a7f74c886dc0cecd4ee17d860a
sgPepper <quentin.bouvet@hotmail.fr>
2016-08-12 18:06:05 +0200

    quick fix

--- 0.components/dependancyResolutionUnit.vhd
+++ 0.components/dependancyResolutionUnit.vhd
@@ -21,15 +21,12 @@ use work.customTypes.all;
 entity FwdPathResolutionUnit is
 GENERIC ( DATASIZE 		: integer; INPUT_NB : integer );
 port(	readAdr 		: in std_logic_vector(4 downto 0); -- adress = vector of size 5
-		wAdrArray 		: in ADDR_ARRAY(INPUT_NB-1 downto 1);							-- (memory bypass, newest -> oldest write addresses)
-		adrValidArray	: in bitArray_t(INPUT_NB-1 downto 0);							-- (memory bypass, newest -> oldest write addresses, readAdr)
+		wAdrArray 		: in ADDR_ARRAY(INPUT_NB-1 downto 1);							-- (newest -> oldest(mem bypass) write addresses)
+		adrValidArray	: in bitArray_t(INPUT_NB-1 downto 0);							-- (newest -> oldest(mem bypass) write addresses, readAdr)
 		
-		-- keep ?
-		-- sel : out std_logic_vector(integer(log2(real(NUM_INPUT_WR_ADDR))) downto 1);
-		
-		inputArray 		: in vectorArray_t(INPUT_NB-1 DOWNTO 0)(DATASIZE-1 downto 0);	-- (memory bypass, forwarding paths in "newest instructions leftmost" order, RF output)
+		inputArray 		: in vectorArray_t(INPUT_NB-1 DOWNTO 0)(DATASIZE-1 downto 0);	-- (newest -> oldest(mem bypass) instruction's results, RF output)
 		output 			: out std_logic_vector(DATASIZE-1 downto 0);		
-		inputValidArray : in bitArray_t(INPUT_NB-1 downto 0);							-- (memory bypass, newwest->oldest instruction result, registerFile)
+		inputValidArray : in bitArray_t(INPUT_NB-1 downto 0);							-- (newest -> oldest(mem bypass) instruction's results, RF output)
 		nReady 			: in std_logic;		
 		valid, ready	: out std_logic	-- only the register file's input had full control signals				
 );
@@ -47,16 +44,13 @@ begin
 	output <= inputArray(0);		-- by default, we select the data read from the register file
 	valid <= inputValidArray(0);
 																											
-	for i in 1 to INPUT_NB-1 loop			-- order of exploration : oldest to newest instructions, then memory bypass
+	for i in 1 to INPUT_NB-1 loop			-- order of exploration : oldest (memoy bypass) to newest instructions
 		if(readAdr = wAdrArray(i)) then		-- if the address we want to read matches with one that is currently being written
 		
 			-- select the correct input
 			output <= inputArray(i);	
 			-- forward its 'valid0' control signal
 			valid <= inputValidArray(i) and adrValidArray(i);
-			
-			-- keep ?
-			--res_sel <= std_logic_vector(to_unsigned(i,integer(log2(real(NUM_INPUT_WR_ADDR)))));
 		end if;
 	end loop;
 	end process;
