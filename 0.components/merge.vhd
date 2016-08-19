5d5053a468251dded9d90e42449178095bb6966b
sgPepper <quentin.bouvet@hotmail.fr>
2016-08-19 12:01:49 +0200

    implemented a version of mergeHybrid that uses a bufferedJoin (still trying to fix circuit)

--- 0.components/merge.vhd
+++ 0.components/merge.vhd
@@ -93,3 +93,58 @@ begin
 	readyArray(0) <= joinReadyArray(0);
 
 end vanilla;
+
+
+
+---------------------------------------------------------------------
+-- mergeHybrid version which uses a bufferedJoin instead of a vanilla 
+-- could help fixing circuit fwdPathResolution
+---------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use work.customTypes.all;
+use work.
+
+entity mergeHybridBuffered is port(
+	clk, reset		: in std_logic;
+	data1, data0 	: in std_logic_vector(31 downto 0);
+	dataOut 		: out  std_logic_vector(31 downto 0);
+	condition		: in std_logic;
+	
+	pValidArray 	: in bitArray_t(2 downto 0);	-- (data1, data0, condition)
+	nReady 			: in std_logic;
+	valid 			: out std_logic;
+	readyArray 		: out bitArray_t(2 downto 0));	-- (data1, data0, condition)
+end mergeHybridBuffered;
+
+architecture vanilla of mergeHybridBuffered is
+	signal mergeValid 		: std_logic;
+	signal joinReadyArray 	: bitArray_t(1 downto 0);
+	signal mergeOut 		: std_logic_vector(31 downto 0);
+	signal joinOut 			: vectorArray_t(1 downto 0)(31 downto 0);	--(oc, mergeOut)
+begin
+
+	mrg : entity work.merge(vanilla)
+			port map(	data1, data0,
+						mergeOut,
+						pValidArray(2 downto 1),
+						joinReadyArray(1),
+						mergeValid,
+						readyArray(2 downto 1));
+						
+	join : entity work.bufferedJoinN(vanilla) generic map (2)
+			port map(	clk, reset,
+						((zero(31 downto 1), condition), mergeOut),	-- dataInt : (oc, mergeOut)
+						joinOut,
+						(mergeValid, pValidArray(0)),	-- pValidArray
+						nReady,
+						valid,
+						joinReadyArray);				-- readyArray : (mergedStuff, condition)
+	
+	-- ready signal for condition
+	readyArray(0) <= joinReadyArray(0);
+	
+	-- we're only interrested in the result of the merge, after it went through the bufferedJoin (recall, 1 cycle delay)
+	dataOut <= joinOut(0);
+
+end vanilla;
